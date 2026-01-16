05d5f2dbe02ba176f373d606adb55abba89dfba6
Hero-Kid <hero.kid1591@gmail.com>
2026-01-16 15:21:39 +0100

    Update c210480001.lua

--- script /CustomHero/c210480001.lua
+++ script /CustomHero/c210480001.lua
@@ -8,9 +8,9 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_TO_HAND)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,{id,0})
 	e1:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_DECK) end)
-	e1:SetCost(s.SelfReveal)
+	--e1:SetCost(s.SelfReveal)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
@@ -19,18 +19,74 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCondition(function(e) return e:GetHandler():IsSummonLocation(LOCATION_HAND) end)
 	e2:SetCode(EVENT_SUMMON_SUCCESS) -- Summoned succesfuly from hand
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(function(e) return e:GetHandler():IsSummonLocation(LOCATION_HAND) end) -- Previously in hand
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--Banish from GY to target 3 "HERO" monsters and shuffle them into deck
+	--Banish from GY, to target 3 "HERO" monsters and shuffle them into deck
 	local e3=Effect.CreateEffect(c)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCost(Cost.AND(Cost.SelfBanish,Cost.HardOncePerChain(id)))
+	e3:SetTarget(s.gytg)
+	e3:SetOperation(s.gyop)
 	c:RegisterEffect(e3)
 end
+
+
+
+
+-- Effect 1
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+-- Effect 2
+function s.thfilter(c)
+	return c:IsSetCard(SET_ELEMENTAL_HERO|SET_DESTINY_HERO|SET_MASKED_HERO|SET_VISION_HERO|SET_XTRA_HERO|SET_CONTRAST_HERO|SET_FAVORITE_HERO) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+end
+-- Effect 3
+function s.gyfilter(c)
+	return c:IsSetCard(SET_ELEMENTAL_HERO|SET_DESTINY_HERO|SET_MASKED_HERO|SET_VISION_HERO|SET_XTRA_HERO|SET_CONTRAST_HERO|SET_FAVORITE_HERO) and c:IsMonster() and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE|LOCATION_BANISH,0,3,nil) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_GRAVE|LOCATION_BANISH)
+endv
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,5,5,nil)
+	if #g~=5 then return end
+	Duel.HintSelection(g)
+	if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==5 then
+		if Duel.GetOperatedGroup():IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
+		Duel.BreakEffect()
+		if Duel.Draw(p,d,REASON_EFFECT)==2 and c:IsRelateToEffect(e) and c:IsFaceup() then
+			c:UpdateAttack(1000)
+		end
+	end
+end
+
+
+
+
 
 
 s.listed_series={0x8,0xc008}
