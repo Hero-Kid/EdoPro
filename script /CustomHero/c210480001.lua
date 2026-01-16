19ec34d16fb7ee28b685a1a42b65d4e69440d1bb
Hero-Kid <hero.kid1591@gmail.com>
2026-01-16 15:23:40 +0100

    Update c210480001.lua

--- script /CustomHero/c210480001.lua
+++ script /CustomHero/c210480001.lua
@@ -62,7 +62,7 @@ function s.thfilter(c)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 -- Effect 3
 function s.gyfilter(c)
